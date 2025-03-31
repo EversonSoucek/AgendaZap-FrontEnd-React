a2a8e3f5821ebb3dcc74c47a925986165c12dc90
EversonSoucek <sousekjuniorr@outlook.com>
2025-03-31 18:20:38 -0300

    Feat: Adiciona estilo de agendamento no calendario

--- host/src/components/Calendario/CalendarioCelula/CalendarioCelula.tsx
+++ host/src/components/Calendario/CalendarioCelula/CalendarioCelula.tsx
@@ -9,6 +9,9 @@ export const CalendarioCelula = ({ dia }: { dia: Date }) => {
         <div className={`calendario-celula ${isToday(dia) ? "hoje" : ""} ${dia.getMonth() === dataSelecionada.getMonth() ? "" : "calendario-celula--fora-mes"} `}>
             <div className={`calendario-celula__dia ${dia.getMonth() === dataSelecionada.getMonth() ? "" : "calendario-celula__dia--fora-mes"}`}>
                 {dia.getDate()}
+                <div className="calendario-celula__agendamentos">
+                    <span className="calendario-celula__agendamento"><div className="ponto"></div >11:00 | Everson</span>
+                </div>
             </div>
         </div>
     )
